90210ce2551ac604912ea35aaf2876b3ea1a16ef
Jorge Gonzalez <jorge.gonzalez.gonzalez@gmail.com>
2018-06-18 21:12:50 +0200

    sliding_bars.sh: directions: right-to-left and left-to-right; fade: in and out

--- sliding_bars.sh
+++ sliding_bars.sh
@@ -4,15 +4,19 @@
 #
 # 	Description:	Script to fade-in fade-out a video using bars of adjustable size that dissapear/appear.
 #
-#	Version:	0.1
+#	Version:	0.4
 #
 #	Modifications:	v0.1; first version.
+#			v0.2; check that input/output dirs are different.
+#			v0.3; bar direction left-to-right and right-to-left.
+#			v0.4; fade out.
 #
-#	Future imprv.:	0.2 bar direction left-to-right and right-to-left; fade out; preview
+#
+#	Future imprv.:	preview; better check of width/bar_numer
 #
 
 #Some variables
-version=0.1
+version=0.4
 identify=$(which identify-im6)
 convert=$(which convert-im6)
 composite=$(which composite-im6)
@@ -28,12 +32,12 @@ if [[ ! -e "${composite}" ]] || [[ ! -e "${convert}" || ! -e "${identify}" ]]; t
 fi
 
 function merge_bars(){
+	echo -e "\nMerging the bars into single images..."
 	#soooo many counters
 	#for sure this should be easier and more elegant
 	rest=1
 	count=1
 	final=1
-	j=1
 	k=1
 
 	process=${bar_number}
@@ -43,9 +47,14 @@ function merge_bars(){
 		let process=${process}-1
 	done
 
-	for i in `seq 1 ${total_bars_processed}`; do
+	posa=1
+	posb=${total_bars_processed}
+	sign="+"
+	j=1
+	processing_bars=${bar_number}
+	for i in `seq ${posa} ${sign}1 ${posb}`; do
 		images="${images} ${output}/bar_${j}_${k}.PNG"
-		let j=${j}+1
+		let j=${j}${sign}1
 		k=1
 		if [[ "${j}" -gt "${bar_number}" ]]; then
 			j=${rest}
@@ -62,9 +71,6 @@ function merge_bars(){
 		images=""
 		fi
 	done
-	
-	#cleanup 
-	#rm bar_*_*.PNG
 }
 
 function slide_bars(){
@@ -77,9 +83,10 @@ function slide_bars(){
 	width=`${identify} ${dir}/${file} | awk '{ print $3 }' | awk -F"x" '{ print $1 }'`
 	height=`${identify} ${dir}/${file} | awk '{ print $3 }' | awk -F"x" '{ print $2 }'`
 
-	total_images_main=`ls -l ${dir} | grep DSC | wc -l`
+	total_images=`ls -l ${dir} | grep DSC | wc -l`
 
-	#create the output directory
+	#delete and create the output directory
+	rm -rf ${output} 2>/dev/null
 	mkdir -p ${output}
 
 	#create the base transparent image
@@ -89,24 +96,39 @@ function slide_bars(){
 	#bar color
 	color=black
 	let bar_width=${width}/${bar_number}
-	xpos1=0
-	xpos2=${bar_width}
+	if [[ "${direction}" == "ltr" ]]; then
+		xpos1=0
+		xpos2=${bar_width}
+		sign="+"
+	elif [[ "${direction}" == "rtl" ]]; then
+		let xpos1=${width}
+		xpos2=${bar_width}
+		xpos2=0
+		sign="-"
+	fi
 	i=0
 	for i in `seq 1 ${bar_number}`; do
+		echo -e -n "\rCreating base image ${i}/${bar_number}"
 		${convert} ${output}/base.PNG -fill ${color} -stroke ${color} -draw "rectangle ${xpos1},0 ${xpos2},${height}" ${output}/bar_tmp_${i}.PNG
-		let xpos1=${xpos1}+${bar_width}
-		let xpos2=${xpos2}+${bar_width}
+		let xpos1=${xpos1}${sign}${bar_width}
+		let xpos2=${xpos2}${sign}${bar_width}
 		let i=${i}+1
 	done
+
+	echo ""
 
 	#create as many bars as needed per fps and bar_number, and apply transparency if necessary
 	perc=`echo "100/(${fps}-1)" | bc`
 	for i in `seq 1 ${bar_number}`; do
-		curr_perc=0
+		echo -e -n "\rCreating bar image ${i}/${bar_number}"
+		if [[ "${transparency}" == "yes" ]]; then
+			sign="+"
+			curr_perc=0
+		fi
 		for j in `seq 1 ${fps}`; do
 			if [[ "${transparency}" == "yes" ]]; then
 				${convert} ${output}/bar_tmp_${i}.PNG -alpha set -channel A -evaluate subtract ${curr_perc}% ${output}/bar_${i}_${j}.PNG
-				let curr_perc=${curr_perc}+${perc}
+				let curr_perc=${curr_perc}${sign}${perc}
 			else
 				cp ${output}/bar_tmp_${i}.PNG ${output}/bar_${i}_${j}.PNG
 			fi
@@ -120,17 +142,34 @@ function slide_bars(){
 	#merge the original image with the necessary layers
 	i=1
 	let total_bars=${bar_number}*${fps}
+	j=${total_bars}
+	let final_bars=${total_images}-${total_bars}
 	for file in `ls -al ${dir} | grep JPG | awk '{ print $9 }'`; do
-		if [[ "${i}" -lt "${total_bars}" ]]; then
-			${convert} ${dir}/${file} -page +0+0 ${output}/bar_${i}.PNG -flatten ${output}/${file}
-		else
-			cp ${dir}/${file} ${output}/${file}
+		if [[ "${fade}" == "in" ]]; then
+			if [[ "${i}" -lt "${total_bars}" ]]; then
+				echo -e -n "\rMerging bar into image ${i}/${file}"
+				${convert} ${dir}/${file} -page +0+0 ${output}/bar_${i}.PNG -flatten ${output}/${file}
+			else
+				echo -e -n "\rCopying original image ${i}/${file}"
+				cp ${dir}/${file} ${output}/${file}
+			fi
+		elif [[ "${fade}" == "out" ]]; then
+			if [[ "${i}" -gt "${final_bars}" ]]; then
+				echo -e -n "\rMerging bar into image ${j}/${file}"
+				${convert} ${dir}/${file} -page +0+0 ${output}/bar_${j}.PNG -flatten ${output}/${file}
+				let j=${j}-1
+			else
+				echo -e -n "\rCopying original image ${i}/${file}"
+				cp ${dir}/${file} ${output}/${file}
+			fi
 		fi
 		let i=${i}+1
 	done
-	
+
 	#cleanup the rest of the PNG files
 	rm ${output}/*.PNG
+
+	echo ""
 }
 
 function version(){
@@ -139,22 +178,34 @@ function version(){
         exit 0
 }
 
+function check_args(){
+	if [[ "${dir}" == "${output}" ]]; then
+		echo "ERROR: input and output directories cannot be the same!"
+		exit 1
+	fi
+	if [[ "${fade}" != "in" && "${fade}" != "out" ]]; then
+		echo "ERROR: fade has to be either \"in\" or \"out\""
+		exit 1
+	fi
+}
+
 function usage(){
         echo -e "\t./$(basename $0) -d <VALUE> -o <VALUE> -b <VALUE> -a <VALUE> -f <VALUE> -t <VALUE> -r <VALUE>"
 	echo -e "\t-d directory where the files are"
 	echo -e "\t-o output directory"
 	echo -e "\t-b OPTIONAL number of separating bars (default ${bar_number})"
 	echo -e "\t-a OPTIONAL frames per bar (default is ${fps})"
-#	echo -e "\t-f OPTIONAL (fade) fade in or fade out (default in)"
-#	echo -e "\t-t OPTIONAL direction of the bars (default left-to-right)"
+	echo -e "\t-f OPTIONAL (fade) fade in or fade out (default in)"
+	echo -e "\t-t OPTIONAL direction of the bars ltr or rtl (default left-to-right)"
 	echo -e "\t-e OPTIONAL apply transparency to the bars"
 #	echo -e "\t-p OPTIONAL (preview) applies the modifications to the first foto to see the result"
-        echo -e "\t-v show version number"
+	echo -e "\t-v show version number"
         echo -e "\t-h show this help"
         exit 0
 }
 
 function main(){
+	check_args
 	slide_bars
 }
 
